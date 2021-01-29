47accb71190acdfa92d7ff1dd1e0a863fa3757ab
MauricioScherer <mauricio.sscherer@gmail.com>
2021-01-29 17:59:44 -0300

    Update

---
+++ ProjetoMemories/Memories_GGJ21/Assets/Main/Script/TriggerNovaSala.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerNovaSala : MonoBehaviour
+{
+    [SerializeField] private GameObject novaSala;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            novaSala.SetActive(true);
+        }
+    }
+}
